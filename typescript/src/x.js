2c139fc4a00617b0229fba945e2e94b32cb4f5e2
madhavsahi <madhavsahi16@gmail.com>
2024-02-09 17:43:52 +0530

    learning postgres

---
+++ typescript/src/x.js
@@ -0,0 +1,24 @@
+const p = new Promise((resolve, reject) => {
+  if (true) {
+    resolve("hey-resolve");
+  } else {
+    reject("hey-reject");
+  }
+});
+async function asyncFxn() {
+  console.log("hey1");
+  const a = 10;
+  console.log(a);
+  console.log("pre1");
+  console.log("pre2");
+  const ans = await p;
+  console.log(ans);
+  console.log("hey4");
+  console.log("hey5");
+  console.log("hey6");
+}
+
+console.log("hey2");
+asyncFxn();
+console.log("hey3");
+console.log("post");
