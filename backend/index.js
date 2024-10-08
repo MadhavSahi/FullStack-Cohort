cb54ab29b773d01e16b12c5c9bd531b4e6587a38
Madhav Sahi <63411260+MadhavSahi@users.noreply.github.com>
2024-10-08 11:35:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.get("/home",(req,res)=>{
     //any logic specific thing we can do here...
     console.log(req.body);
     res.status(200).json({
-        text:"Hi madhav sahi",
+        text:"Hi madhav Sahi",
     });
 });
 
