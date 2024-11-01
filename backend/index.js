424f895fc5a9596c17ed838ff90300dcd2d1f783
Madhav Sahi <63411260+MadhavSahi@users.noreply.github.com>
2024-11-01 23:58:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.get("/home",(req,res)=>{
     //any logic specific thing we can do here...
     console.log(req.body);
     res.status(200).json({
-        text:"Hi Madhav Sahi",
+        text:"Hi Madhav Sahi !",
     });
 });
 
